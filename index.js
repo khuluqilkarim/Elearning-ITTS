eecf3ee355767c876a5dd0f84975cb45f4183037
khuluqilkarim <mpherex24@gmail.com>
2023-05-07 20:24:07 +0700

    e-learning

--- index.js
+++ index.js
@@ -2,23 +2,42 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 
+let hasilTerakhir; // nampung nilai yang di load selama 1 menit
+
 app.get('/tugas', async (req, res) => {
-  const hasil = await Cari_tugas();
-  res.send(hasil);
+  // Jika hasilTerakhir kosong, panggil fungsi Cari_tugas untuk mendapatkan hasil terbaru
+  if (!hasilTerakhir) {
+    const nilai = await Cari_tugas();
+    return res.send(nilai); 
+  }
+
+  // Kirim hasilTerakhir jika sudah tersedia
+  if (hasilTerakhir) {
+    return res.send(JSON.stringify(hasilTerakhir));
+  } else {
+    // Jika hasilTerakhir masih kosong, kirimkan pesan error
+    return res.status(500).send('Gagal mendapatkan hasil tugas terbaru');
+  }
 });
+
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
+setInterval(async () => {
+  hasilTerakhir = await Cari_tugas();
+  console.log('Hasil terakhir:', hasilTerakhir);
+}, 60 * 1000); // 1 menit dalam milidetik
+
 async function Cari_tugas() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'], cache: false });
   const page = await browser.newPage();
 
   await page.goto('https://elearning.ittelkom-sby.ac.id/login/index.php'); 
 
-  await page.type('input[name="username"]', 'ISI DENGAN NIM');
-  await page.type('input[name="password"]', 'ISI DENGAN PASSWD');
+  await page.type('input[name="username"]', '1205210024');
+  await page.type('input[name="password"]', 'uzul2003');
   await Promise.all([
     page.waitForNavigation(),
     page.click('button[type="submit"]')
@@ -26,21 +45,17 @@ async function Cari_tugas() {
 
   await page.goto('https://elearning.ittelkom-sby.ac.id/calendar/view.php'); 
 
-  const col11Elements = await page.$$eval('.calendarwrapper .eventlist .event .card .description .row .col-xs-11', elements => {
-    return elements.map(element => element.textContent.trim());
+  const data = await page.$$eval('.calendarwrapper .eventlist .event .card', elements => {
+    return elements.map(element => {
+      const title = element.querySelector('h3').textContent.trim();
+      const description = element.querySelector('.description .row .col-xs-11').textContent.trim();
+      return { title, description};
+    });
   });
-
-  const event = col11Elements[1];
-  const tugas = "Tugas : " + col11Elements[2];
-  const deadline = "Deadline : " + col11Elements[0];
   
-  const hasil = {
-    event,
-    tugas,
-    deadline
-  };
-
   await browser.close();
 
-  return hasil;
+  const json = JSON.stringify(data);
+
+  return json;
 }
